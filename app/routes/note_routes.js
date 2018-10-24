f9368e9e7cef7ba3a6dd842e1664018b8e72a06f
fchavat <felipe.chavat@gmail.com>
2018-10-24 13:26:39 -0300

    Add insert note on post notes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,19 @@
 module.exports = function(app, db) {
   app.post('/notes', (req, res) => {
     console.log(req.body);
-    res.send('Your are in /notes');
+
+    const note = {
+      title: req.body.title,
+      body: req.body.body
+    };
+
+    db.collection('notes').insertOne(note, (err, result) => {
+      if (err) {
+        res.send({ 'error': 'An error has ocurred' });
+      } else {
+        res.send(result.ops[0]);
+      };
+    });
   });
   console.log('[i] note_routes exported succesfully');
 };
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express     = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser  = require('body-parser');
+const db          = require('./config/db');
 
 const app         = express();
 
@@ -8,7 +9,15 @@ const port        = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-require('./app/routes')(app, {});
-app.listen(port, () => {
-  console.log(`Listening on port ${port}...`);
-});
+MongoClient.connect(db.url, { useNewUrlParser: true }, (err, database) => {
+  if (err) return console.log(`Error::::${err}`);
+
+  console.log(`[i] Succesfully connected to mongoDB`);
+
+  const db = database.db('node-api-example');
+  require('./app/routes')(app, db);
+
+  app.listen(port, () => {
+    console.log(`[i] Listening on port ${port}...`);
+  });
+})

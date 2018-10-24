37c2b9dcba9450a9f18bba38fe3f0ad22c51f81a
fchavat <felipe.chavat@gmail.com>
2018-10-24 16:32:36 -0300

    Add routes for CRUD notes

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,12 +1,53 @@
-module.exports = function(app, db) {
-  app.post('/notes', (req, res) => {
-    console.log(req.body);
+var ObjectID = require('mongodb').ObjectID;
 
+module.exports = function(app, db) {
+
+  app.get('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectID(id) };
+    db.collection('notes').findOne(details, (err, item) => {
+      if (err) {
+        res.send({ 'error': err });
+      } else {
+        res.send(item);
+      }
+    });
+  });
+
+  app.put('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectID(id) };
+    const note = {
+      title: req.params.title,
+      body: req.params.body
+    };
+    db.collection('notes').updateOne(details, { $set: note }, (err, result) => {
+      if (err) {
+        console.log(`Error:::: ${err}`)
+        res.send({ 'error': err });
+      } else {
+        res.send(note);
+      }
+    });
+  });
+
+  app.delete('/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const details = { _id: new ObjectID(id) };
+    db.collection('notes').deleteOne(details, (err) => {
+      if (err) {
+        res.send({ 'error': err });
+      } else {
+        res.send(`Note ${id} successfully deleted`)
+      }
+    });
+  });
+
+  app.post('/notes', (req, res) => {
     const note = {
       title: req.body.title,
       body: req.body.body
     };
-
     db.collection('notes').insertOne(note, (err, result) => {
       if (err) {
         res.send({ 'error': 'An error has ocurred' });
@@ -15,5 +56,5 @@ module.exports = function(app, db) {
       };
     });
   });
-  console.log('[i] note_routes exported succesfully');
+  console.log('[i] note_routes exported successfully');
 };
